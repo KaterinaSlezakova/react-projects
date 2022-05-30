86188092a7c6e73a94e733f6ba0f945c7acf0e4c
Katerina S <kslezakova12@gmail.com>
2022-05-30 22:43:18 +0200

    practising on projects

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,44 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState("0");
+  const [text, showText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    showText(data.slice(0, amount));
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <div className="section-center">
+      <h3>lorem ipsum project setup</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          value={count}
+          name="amount"
+          id="amount"
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((article, index) => {
+          return <p key={index}>{article}</p>;
+        })}
+      </article>
+    </div>
+  );
 }
 
 export default App;
--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -1,9 +1,92 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id == id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.name);
+  };
+
+  const removeItem = (id) => {
+    showAlert(true, "item has been removed", "danger");
+    const newList = list.map((item) => item.id !== id);
+    setList(newList);
+  };
+
+  const clearList = (e) => {
+    showAlert(true, "empty list", "danger");
+    setList([]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      showAlert(true, "please enter item", "danger");
+    } else if (name && isEditing) {
+      setList(
+        list.map((item) => {
+          if (item.id == editID) {
+            return { ...list, name };
+          }
+          return item;
+        })
+      );
+      setName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "item has been edited", "success");
+    } else {
+      const newItem = { id: new Date().getTime().toString(), name };
+      setList([...list, newItem]);
+      showAlert(true, "item added to the list", "success");
+      setName("");
+    }
+  };
+  const showAlert = (show = false, msg = "", type = "") => {
+    return setAlert({ show, msg, type });
+  };
+
+  return (
+    <>
+      <section className="section-center">
+        <form className="grocery-form" onSubmit={handleSubmit}>
+          {alert.show && (
+            <Alert {...alert} removeAlert={showAlert} list={list} />
+          )}
+          <h3>grocery bud</h3>
+          <div className="form-control">
+            <input
+              type="text"
+              className="grocery"
+              placeholder="e.g. milk"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button type="submit" className="submit-btn">
+              {isEditing ? "edit" : "submit"}
+            </button>
+          </div>
+        </form>
+        {list.length > 0 && (
+          <div className="grocery-container">
+            <List items={list} removeItem={removeItem} editItem={editItem} />
+            <button type="submit" className="clear-btn" onClick={clearList}>
+              clear items
+            </button>
+          </div>
+        )}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
